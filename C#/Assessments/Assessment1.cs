7b93f86bad817be5d2136ba86346b622fae9ad8b
Sahana prabakaran <sahanap@infinite.com>
2024-11-06 10:49:07 +0530

    Assessment1

---
+++ C#/Assessments/Assessment1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program1
+            Console.WriteLine("Enter the string");
+            string word = Console.ReadLine();
+            Console.WriteLine("Enter the index");
+            int index = Convert.ToInt32(Console.ReadLine());
+            removechar(word, index);
+
+
+            //Program2
+            Console.WriteLine("Enter the string");
+            string word1 = Console.ReadLine();
+            if (word1.Length <= 1)
+                Console.WriteLine(word1);
+            else
+            { 
+                char firstcharacter  = word1[0];
+                char lastcharacter = word1[word1.Length - 1];
+                Console.WriteLine(lastcharacter + word1.Substring(1, word1.Length - 2) + firstcharacter);
+
+            }
+
+
+            //Program3
+            Console.WriteLine("Enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 && num1 > num3)
+                Console.WriteLine("Output: " + num1);
+            if (num2 > num1 && num2 > num3)
+                Console.WriteLine("Output: " + num2);
+            else
+                Console.WriteLine("Output: " + num3);
+
+
+            //Program4
+            Console.WriteLine("Enter the string");
+            string word2 = Console.ReadLine();
+            Console.WriteLine("Enter the letter");
+            char letter = Convert.ToChar(Console.ReadLine());
+            int count = 0;
+            for(int i =0;i<word2.Length;i++)
+            {
+                if (letter == word2[i])
+                    count++;
+            }
+            Console.WriteLine("The number of times it has occured is " + count);
+
+        }
+
+       
+        //Function of Program1
+        static void removechar(string word, int index)
+        {
+            for(int i=0;i<word.Length;i++)
+            {
+                if (i != index)
+                    Console.Write(word[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Assignments/Assignment3/Assignment3_Strings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StringPrograms
+
+            Console.WriteLine("Length of a word program");
+            Console.WriteLine("Enter a word");
+            string word1 = Console.ReadLine();
+            int length = word1.Length;
+            Console.WriteLine("The length of the word is {0}", length);
+
+
+            Console.WriteLine("Rverse of a word program");
+            Console.WriteLine("Enter a word");
+            string word2 = Console.ReadLine();
+            string reverseword = new String (word2.Reverse().ToArray());
+            Console.WriteLine("The reversed string is " + reverseword);
+
+
+            Console.WriteLine("Compare two words program");
+            Console.WriteLine("Enter the first word");
+            string word3 = Console.ReadLine();
+            Console.WriteLine("Enter the second word");
+            string word4 = Console.ReadLine();
+            if(word3.ToLower()==word4.ToLower())
+            {
+                Console.WriteLine("The words are the same");
+            }
+            else
+            {
+                Console.WriteLine("The words are not the same");
+            }
+        }
+    }
+}
