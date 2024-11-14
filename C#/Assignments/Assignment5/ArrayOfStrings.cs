e13d2e4ca1813f61e6f91584c4847c9d7623daf6
Sahana prabakaran <sahanap@infinite.com>
2024-11-14 22:27:10 +0530

    Assignment5

---
+++ C#/Assignments/Assignment5/ArrayOfStrings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace File_ArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number of lines you want to enter");
+            int linesCount = Convert.ToInt32(Console.ReadLine());
+            string[] lines = new string[linesCount];
+            for (int i = 0; i < linesCount; i++)
+            {
+                Console.WriteLine("Enter a String");
+                lines[i] = Console.ReadLine();
+            }
+
+            FileStream fs = new FileStream("SampleFile.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            for(int i = 0; i < linesCount; i++)
+            {
+                sw.WriteLine(lines[i]);
+            }
+     
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+        }
+    }
+}
---
+++ C#/Assignments/Assignment5/CountLines.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace File_CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filepath = "SampleFile.txt";
+                if (File.Exists(filepath))
+                {
+                    int lineCount = 0;
+                    using (StreamReader reader = new StreamReader(filepath))
+                    {
+                        while (reader.ReadLine() != null)
+                        {
+                            lineCount++;
+                        }
+                    }
+                    Console.WriteLine("The number of lines in the file is: " + lineCount);
+                }
+                else
+                {
+                    Console.WriteLine("The file does not exist");
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.Message);
+
+            }
+        }
+    }
+}
